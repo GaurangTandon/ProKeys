809204d5c269b800cbe0fc6be1668e6c963b9569
Gaurang Tandon <1gaurangtandon@gmail.com>
2017-01-08 19:34:23 +0530

    changed the file code

--- js/temp_nav_switcher_code.js
+++ js/temp_nav_switcher_code.js
@@ -1,36 +1,30 @@
 // TEMPORARY
-// previous code of navigation bar switcher of dual textbox might help when we implement "preview" snippets feature
-$nav.on("click", (function(currObj){  // preserve ref without `bind`ing handler		
-		return function(e){
-			var node = e.target, parent, // event delegation
-				newlyShownEditor;
-				
-			if(!(node.tagName == "P") ||
-				// only show if not already shown
-				node.hasClass(SHOW_CLASS)) return true;	
-
-			parent = this.parentNode;
-			// remove show class from `p` and corresponding box
-			parent.querySelectorAll("." + SHOW_CLASS).removeClass(SHOW_CLASS);
-			
-			// add show class to `p` and corresponding box
-			node.addClass(SHOW_CLASS);
-			newlyShownEditor = parent.querySelector(node.dataset.selector);
-			newlyShownEditor.addClass(SHOW_CLASS);
-			newlyShownEditor.setAttribute("tab-index", )
-			
-			if(transferContentsToShownEditor){
-				// <b> tags get converted to bold formatted text
-				// and vice-versa
-				// (mode still not reversed because have to use getPlain"Shown"Text;
-				// so if isCurrModePlain => text taken from curr textarea and pasted
-				// into rich editor)
-				if(isCurrModePlain)
-					currObj.setRichText(convertBetweenHTMLTags(currObj.getPlainShownText(), true));
-				else currObj.setPlainText(currObj.getRichShownText());
-			}
-			
-			// finally reverse the mode
-			isCurrModePlain = !isCurrModePlain;
-		};
-	})(this));
+this.switchToRichEditor = function(){
+		// remove show class from `p` and corresponding box
+		$container.querySelectorAll("." + SHOW_CLASS).removeClass(SHOW_CLASS);
+		
+		// add show class to `p` and corresponding box
+		$pRich.addClass(SHOW_CLASS);
+		$textarea.addClass(SHOW_CLASS);
+		
+		if(transferContentsToShownEditor)
+			this.setPlainText(this.getRichShownText());			
+		
+		// finally reverse the mode
+		isCurrModePlain = !isCurrModePlain;
+	};
+	
+	this.switchToTextarea = function(){
+		// remove show class from `p` and corresponding box
+		$container.querySelectorAll("." + SHOW_CLASS).removeClass(SHOW_CLASS);
+		
+		// add show class to `p` and corresponding box
+		$pTextarea.addClass(SHOW_CLASS);
+		$textarea.addClass(SHOW_CLASS);
+		
+		if(transferContentsToShownEditor)
+			this.setPlainText(this.getRichShownText());			
+		
+		// finally reverse the mode
+		isCurrModePlain = !isCurrModePlain;
+	};
