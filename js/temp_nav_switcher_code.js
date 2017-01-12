51cbab94b27b6d1094f860ec2fab83f80ae691d0
Gaurang Tandon <1gaurangtandon@gmail.com>
2017-01-12 17:00:18 +0530

    Update temp_nav_switcher_code.js

--- js/temp_nav_switcher_code.js
+++ js/temp_nav_switcher_code.js
@@ -1,30 +1,30 @@
-// TEMPORARY
-this.switchToRichEditor = function(){
+
+	this.switchToTextarea = function(transferContentsToShownEditor){
 		// remove show class from `p` and corresponding box
-		$container.querySelectorAll("." + SHOW_CLASS).removeClass(SHOW_CLASS);
-		
-		// add show class to `p` and corresponding box
-		$pRich.addClass(SHOW_CLASS);
-		$textarea.addClass(SHOW_CLASS);
-		
-		if(transferContentsToShownEditor)
-			this.setPlainText(this.getRichShownText());			
-		
-		// finally reverse the mode
-		isCurrModePlain = !isCurrModePlain;
-	};
-	
-	this.switchToTextarea = function(){
-		// remove show class from `p` and corresponding box
-		$container.querySelectorAll("." + SHOW_CLASS).removeClass(SHOW_CLASS);
+		$pRich.removeClass(SHOW_CLASS);
+		$richEditorContainer.removeClass(SHOW_CLASS);
 		
 		// add show class to `p` and corresponding box
 		$pTextarea.addClass(SHOW_CLASS);
-		$textarea.addClass(SHOW_CLASS);
+		$textarea.addClass(SHOW_CLASS).setAttribute("tab-index", 20);
+		
+		isCurrModePlain = !isCurrModePlain;
 		
 		if(transferContentsToShownEditor)
-			this.setPlainText(this.getRichShownText());			
+			this.setPlainText(this.getRichText());		
+	};
+	
+	this.switchToRichEditor = function(transferContentsToShownEditor){
+		// remove show class from `p` and corresponding box
+		$pTextarea.removeClass(SHOW_CLASS);
+		$textarea.removeClass(SHOW_CLASS);
 		
-		// finally reverse the mode
+		// add show class to `p` and corresponding box
+		$pRich.addClass(SHOW_CLASS);
+		$richEditorContainer.addClass(SHOW_CLASS);
+		
 		isCurrModePlain = !isCurrModePlain;
+		
+		if(transferContentsToShownEditor)
+			this.setRichText(convertBetweenHTMLTags(this.getPlainText(), true));		
 	};
