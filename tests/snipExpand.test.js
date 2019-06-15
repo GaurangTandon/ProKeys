681784aaf9ccea7765d41e8fc137498fa6f2308a
YoogottamK <yoogottamk@outlook.com>
2019-06-15 08:09:54 +0530

    Delimited snippets tested

--- tests/snipExpand.test.js
+++ tests/snipExpand.test.js
@@ -1,10 +1,21 @@
+const fs = require("fs"),
+    path = require("path");
 /*
  * add this since jest's default timeout is 5s only
  * Loading a page might take longer
  */
-
-const JEST_TIMEOUT = 30000,
-    extensionDelay = 300;
+const JEST_TIMEOUT = 60000,
+    extensionDelay = 300,
+    extSettings = opts => "{\"snippets\":[\"Snippets\",1560477878650],"
+    + "\"blockedSites\":[],"
+    + "\"charsToAutoInsertUserList\":"
+    + "[[\"(\",\")\"],[\"{\",\"}\"],[\"\\\"\",\"\\\"\"],[\"[\",\"]\"]],"
+    + "\"dataVersion\": 1,\"language\": \"English\",\"hotKey\": [\"shiftKey\",32],"
+    + "\"dataUpdateVariable\": false,\"matchDelimitedWord\":"
+    + `${opts.matchDelimitedWord},"tabKey": ${opts.tabKeyExpandSpace},`
+    + "\"snipNameDelimiterList\":\"@#$%&*+-=(){}[]:\\\"'/_<>?!., \","
+    + "\"omniboxSearchURL\":\"https://www.google.com/search?q=SEARCH\","
+    + "\"wrapSelectionAutoInsert\":true,\"ctxEnabled\":true}";
 
 jest.setTimeout(JEST_TIMEOUT);
 
@@ -27,11 +38,13 @@ const testURLs = [
             snipText: "abrbc",
             expansion: "abe right backc",
             cursorChange: "h",
+            delimitedExpansion: "abrb c",
         },
         {
             snipText: "brb",
             expansion: "be right back",
             cursorChange: "",
+            delimitedExpansion: "be right back",
         },
     ];
 /**
@@ -125,20 +138,47 @@ async function getExpandedSnippet(
     return expandedText;
 }
 
+/*
+ * function to dismiss all dialogs
+ */
+
+async function dismissDialog(dialog) {
+    await dialog.accept();
+}
+
 /**
  * How do parallel tests work?
  * The following `beforeAll` sends a `gotoURL` call to all of the pages
  * at once. The associated page objects and their `gotoURL` Promises are stored here
  */
-const usablePages = [];
+const usablePages = [],
+    optionsPageTitle = "ProKeys | Options";
 let extWelcomePage;
 
 beforeAll(async () => {
+    const allPages = await browser.pages();
+
+    // .find() method is synchronous, so it has to return a value immediately
+    // so if it asynchronous fn argument, it probably acts weird
+    // hence use for loop
+    for (const pg of allPages) {
+    // eslint-disable-next-line no-await-in-loop
+        const title = await pg.title();
+        if (title === optionsPageTitle) {
+            extWelcomePage = pg;
+            break;
+        }
+    }
+
+    // dismiss dialog on page
+    extWelcomePage.on("dialog", dismissDialog);
+
     for (const testURL of testURLs) {
-        // eslint-disable-next-line no-await-in-loop
+    // eslint-disable-next-line no-await-in-loop
         const usablePage = await browser.newPage();
         // eslint-disable-next-line no-await-in-loop
         await usablePage.setViewport({ width: 1920, height: 1080 });
+        usablePage.on("dialog", dismissDialog);
         usablePages.push({
             usablePage,
             loadedPromise: usablePage.goto(testURL.url),
@@ -146,61 +186,119 @@ beforeAll(async () => {
     }
 });
 
-/**
- * Options page is launched by ext automatically on install
- * Need to search for it
- */
-describe("Open options page", () => {
-    const optionsPageTitle = "ProKeys | Options";
-    beforeAll(async () => {
-        const allPages = await browser.pages();
+async function updateSettings(newSettings) {
+    const settingsFile = "extSettings.txt",
+        extUploadFileFormID = "#file_input_elm",
+        settingsPath = path.join(__dirname, settingsFile),
+        importButtonQuerySelector = "button[class='import']",
+        restoreButtonQuerySelector = "button[class='restore']";
 
-        // .find() method is synchronous, so it has to return a value immediately
-        // so if it asynchronous fn argument, it probably acts weird
-        // hence use for loop
-        for (const pg of allPages) {
-            // eslint-disable-next-line no-await-in-loop
-            const title = await pg.title();
-            if (title === optionsPageTitle) {
-                extWelcomePage = pg;
-                break;
-            }
-        }
-
-        await extWelcomePage.bringToFront();
+    // write the settings file
+    fs.writeFile(settingsPath, newSettings, (err) => {
+        expect(err).toBeFalsy();
     });
 
-    it("Should load prokeys options page", async () => {
-        const title = await extWelcomePage.title();
-        await expect(title).toBe(optionsPageTitle);
+    // bring it to front
+    await extWelcomePage.bringToFront();
+
+    // click the import button
+    const importButton = await extWelcomePage.$(importButtonQuerySelector);
+    await importButton.click();
+
+    // upload the file
+    const uploadFileForm = await extWelcomePage.$(extUploadFileFormID);
+    await uploadFileForm.uploadFile(settingsPath);
+    const restoreButton = await extWelcomePage.$(restoreButtonQuerySelector);
+    await restoreButton.click();
+
+    // reload options page
+    await extWelcomePage.reload();
+
+    // remove the settings file
+    fs.unlink(settingsPath, (err) => {
+        expect(err).toBeFalsy();
+    });
+}
+
+/*
+ * TEST:
+ *  check if snippets are expanding normally
+ */
+describe("Test snippet expansion", () => {
+    testURLs.forEach(({ url, textBoxQueryString }, index) => {
+        let usablePage,
+            loadedPromise;
+
+        beforeAll(async () => {
+            ({ usablePage, loadedPromise } = usablePages[index]);
+            await loadedPromise;
+            // unless we bring it to front, it does not activate snippets
+            await usablePage.bringToFront();
+        });
+
+        describe(`Snipppet expands on ${
+            url.match(/https?:\/\/(\w+\.)+\w+/)[0]
+        }`, () => {
+            testSnippets.forEach(({ snipText, expansion, cursorChange }) => {
+                it(`${snipText} should expand`, async () => {
+                    const expandedText = await getExpandedSnippet(
+                        usablePage,
+                        textBoxQueryString,
+                        snipText,
+                        cursorChange,
+                    );
+                    await expect(expandedText).toBe(expansion);
+                });
+            });
+        });
     });
 });
 
-
-// Test snip expansion
-testURLs.forEach(({ url, textBoxQueryString }, index) => {
-    let usablePage,
-        loadedPromise;
-
+/*
+ * TEST:
+ *  check if snippets obey delimited settings
+ */
+describe("Test delimited snippets", () => {
+    // enable delimited in settings
     beforeAll(async () => {
-        ({ usablePage, loadedPromise } = usablePages[index]);
-        await loadedPromise;
-        // unless we bring it to front, it does not activate snippets
-        await usablePage.bringToFront();
+        await extWelcomePage.bringToFront();
+        await updateSettings(
+            extSettings({ matchDelimitedWord: true, tabKeyExpandSpace: false }),
+        );
     });
 
-    describe(`Snipppet expands on ${
-        url.match(/https?:\/\/(\w+\.)+\w+/)[0]
-    }`, () => {
-        testSnippets.forEach(({ snipText, expansion, cursorChange }) => {
-            it(`${snipText} should match`, async () => {
-                const expandedText = await getExpandedSnippet(
-                    usablePage,
-                    textBoxQueryString,
-                    snipText,
-                    cursorChange,
-                );
-                await expect(expandedText).toBe(expansion);
+    // disable delimited in settings
+    afterAll(async () => {
+        await extWelcomePage.bringToFront();
+        await updateSettings(
+            extSettings({ matchDelimitedWord: false, tabKeyExpandSpace: false }),
+        );
+    });
+
+    testURLs.forEach(({ url, textBoxQueryString }, index) => {
+        let usablePage;
+
+        beforeAll(async () => {
+            ({ usablePage } = usablePages[index]);
+            // unless we bring it to front, it does not activate snippets
+            await usablePage.bringToFront();
+            await usablePage.reload();
+        });
+
+        describe(`Snipppet expands on ${
+            url.match(/https?:\/\/(\w+\.)+\w+/)[0]
+        }`, () => {
+            // for ext changes
+            testSnippets.forEach(({ snipText, cursorChange, delimitedExpansion }) => {
+                it(`${snipText} should become ${delimitedExpansion}`, async () => {
+                    const expandedText = await getExpandedSnippet(
+                        usablePage,
+                        textBoxQueryString,
+                        snipText,
+                        cursorChange,
+                    );
+                    await expect(expandedText).toBe(delimitedExpansion);
+                });
             });
         });
     });
