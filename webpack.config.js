beb10b4b42e58da89baaa955f00cc4e6df304e41
GaurangTandon <1gaurangtandon@gmail.com>
2019-06-26 08:54:33 +0530

    do not transpile in dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const moduleObject = {
+const MODE = "development";
+let moduleObject = {
     rules: [
         {
             test: /\.js$/,
@@ -13,13 +14,15 @@ const moduleObject = {
     ],
 };
 
+if (MODE === "development") { moduleObject = undefined; }
+
 // webpack will only transform all import/export directives
 // gulp will minify all the files
 module.exports = {
     // development mode ensures terser (es6 minifier) is not run
     // see https://webpack.js.org/configuration/mode
     module: moduleObject,
-    mode: "development",
+    mode: MODE,
     entry: {
         background: `${__dirname}/js/background.js`,
         detector: `${__dirname}/js/detector.js`,
